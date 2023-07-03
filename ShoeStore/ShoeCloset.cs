fa742c17dd6a961bac167d77f3a96324c655246e
chajus1 <chajus1@googlemail.com>
2023-07-03 22:44:29 +0100

    additions

---
+++ ShoeStore/ShoeCloset.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoeStore
+{
+    class ShoeCloset
+    {
+        private readonly List<Shoe> shoes = new List<Shoe>();
+
+        public void PrintShoes()
+        {
+            if (shoe.Count == 0)
+            {
+                Console.WriteLine("\nThe shoe closet is empty.");
+            }
+            else
+            {
+                Console.WriteLine("\nThe shoe closet contains:");
+                int i = 1;
+                foreach(Shoe shoe in shoes)
+                {
+                    Console.WriteLine($"Shoe #{i++}: {shoe.Description}");
+                }
+            }
+        }
+    }
+}
